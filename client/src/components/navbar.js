33b1030765acae2cd90dd86526a166c6e71db005
Lewi Gilamichael <dr.lewi@gmail.com>
2017-09-15 10:40:53 -0400

    added note for userName

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -9,7 +9,7 @@ export class NavBar extends React.Component {
   render () {
     return (
       <nav >
-        <h2 className="userInfo">{this.props.name}</h2>
+        <h2 className="userInfo">user name here{this.props.name}</h2>
         <button className="logout"> <a href={'/api/auth/logout'}>LogOut</a> </button>
       </nav>
     )
