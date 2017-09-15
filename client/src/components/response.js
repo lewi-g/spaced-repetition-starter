e5bfe60f573c0dc3887c2cb3911355b13788f690
Lewi Gilamichael <dr.lewi@gmail.com>
2017-09-15 13:07:28 -0400

    attempting to submit response

--- client/src/components/response.js
+++ client/src/components/response.js
@@ -1,35 +1,45 @@
 import React from 'react';
 import * as Cookies from 'js-cookie';
+import * as actions from '../actions';
+
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state, props) => ({
+    question: state.question,
+    name: state.name,
+    picture: state.picture,
+    correct: state.correct,
+    answer: state.answer,
+    answered: state.answered,
+    score: state.score,
+    negScore: state.negScore,
+})
 
 
-export default class Response extends React.Component {
+export class Response extends React.Component {
 
-  onSubmit() {
-    const accessToken = Cookies.get('accessToken');
-    //linking to post endpoint
-    fetch('/api/questions/next', {
-      method: 'post',
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    })
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(res.statusText)
-      }
-      console.log('we are going somewhere');
-      return res.json();
-    })
-
+  onSubmit(e) {
+    e.preventDefault();
+            let formData = {
+          answer: this.userAnswer.value
+        }
+    console.log('answering questions!! yaya!')
+    this.props.dispatch(actions.submitAnswer(formData))
   }
+
   render () {
     return (
        <div className='response'>
-          <form>
-            <input type="text" autoComplete="on" id="response"></input>
-            <button onClick={() => this.onSubmit()} className="btn">Submit response</button>
+          <form id="input-form" onSubmit={this.onSubmit} ref={ref => this.answerForm=ref}>
+            <input type="text"
+              id="user-answer" 
+              ref={ref => this.userAnswer = ref}
+              autoComplete="on"></input>
+            <button type="submit" className="btn">Submit response</button>
           </form>
         </div>
     )
   }
 }
+
+export default connect(mapStateToProps)(Response);
